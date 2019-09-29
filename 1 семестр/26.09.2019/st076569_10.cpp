127252cbef897cfdb075c6e6f74390bd6a74e31a
st076569 <55051748+st076569@users.noreply.github.com>
2019-09-29 21:00:04 +0300

    Add files via upload

---
+++ 1 семестр/26.09.2019/st076569_10.cpp
@@ -0,0 +1,57 @@
+﻿// Напишите программу перебирающую все перестановки массива букв в лексикографическом порядке.
+// Программа должна работать не более чем за O(n!*n) шагов.
+
+#include <iostream>
+using namespace std;
+
+const int count = 4;			// Количество элементов в массиве
+
+bool in_mas (int x, int n, int* a)	// Функция проверки нахождения числа "х" в массиве "а" до позиции "n"
+{
+  bool isinside = false;
+    for (int i = 0; i <= n; i++)
+    {
+      if (x == a[i]) 
+      {
+        isinside = true;
+      }
+    }
+    return isinside;
+}
+
+void translate (int* a)			// Перевод чисел в буквы по таблице ASCII и вывод массива на экран
+{
+  int shift = 97;
+    cout << "( ";
+    for (int i = 0; i <= count - 1; i++)
+    {
+      cout << char(a[i] + shift) << " ";
+    }
+    cout << ")" << endl;
+}
+
+void F (int num, int pos, int* a)	// Рекурсивная процедура
+{
+    if (num >= 0) 
+    {
+      a[pos] = num;
+    }
+    for (int i = 0; i <= count - 1; i++)
+    {
+      if (!in_mas(i, pos, a)) 
+      {
+        F(i,pos+1,a);
+      }
+    }
+    if (pos == count - 1)
+    {
+      translate(a);
+    }
+}
+
+int main ()
+{
+  int mas[count];			// В массиве хранятся числа (0..count-1)
+    F(-1,-1,mas);			// Буквы появляются только при выводе на экран
+    return 0;
+}
---
+++ 1 семестр/26.09.2019/st076569_9.cpp
@@ -0,0 +1,67 @@
+﻿// Напишите эффективную программу, находящую все числа длины n являющиеся палиндромами.
+// В комментариях напишите, почему перебор - эффективный.
+
+// Данный алгоритм эффективный, потому что количество обрабатываемых значений 
+// ассимптотически равняется размеру ответа.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void out_turn (int a)		// Процедура вывода перевернутого числа "а"
+{
+  int b = 0;
+    while (a > 0)
+    {
+      cout << a % 10;
+      a /= 10;
+    }
+    cout << "; ";
+}
+
+void palindrome (int n)		// Процедура вывода всех палиндромов длины "n"
+{
+  int min = 1, max = 0;
+    for (int i = 1; i <= n / 2 - 1; i++)
+    {
+      min *= 10; 
+    }    
+    max = min * 10 - 1;
+    if (n % 2 == 0 && n > 1)
+    {
+      for (int i = min; i <= max; i++)
+      {
+        cout << i; 
+        out_turn(i);
+        if ((i - min + 1) % 10 == 0) cout << endl;
+      }
+    } else
+    if (n > 1)
+    {
+      for (int i = min; i <= max; i++)
+      {
+        for (int j = 0; j <= 9; j++)
+        {
+          cout << i << j;
+          out_turn(i);
+        }
+        cout << endl;
+      }
+    } else
+    {
+      for (int j = 0; j <= 9; j++)
+        {
+          cout << j << "; ";
+        }
+    }
+}
+
+int main ()
+{
+  int n;
+    cout << "Write 'n' :" << endl;
+    cin >> n; 
+    n = abs(n);
+    palindrome(n);
+    return 0;
+}
