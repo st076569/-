74a510c401e4a5bf189aaf5d852fb5bc0b3520ce
st076569 <55051748+st076569@users.noreply.github.com>
2019-12-20 19:52:12 +0300

    Add files via upload

--- 1 семестр/ExtraTasks/st076569_31.cpp
+++ 1 семестр/ExtraTasks/st076569_31.cpp
@@ -8,18 +8,18 @@
 using namespace std;
 
 const int D = 1;                // Размерность пространства
-const int repeat_num = 5;       // Число повторений алгоритма
+const int Repeat_num = 5;       // Число повторений алгоритма
 const double T_end = 1;         // Конечная температура
 const double T_start = 20;      // Начальная температура
-const double Pi = 3.14159265;
+const double Pi = 3.14159265;   // Число Пи
 
 struct PartialArgument
 {
-    double val;     // Аргумент в данном измерении
-    double A, B;    // Начало и конец области поиска
+    double val;                 // Аргумент в данном измерении
+    double A, B;                // Начало и конец области поиска
 };
 
-struct Argument
+struct Argument                 // Вектор
 {
     PartialArgument coord[D];
 };
@@ -33,6 +33,7 @@ double Function3(Argument v)    // Имитация голоса 1
 {
     double ampl1 = (abs(sin(v.coord[0].val / 8)) + 1);
     double ampl2 = (abs(cos(v.coord[0].val + Pi * (abs(cos(v.coord[0].val)) + 1))) + 1) * 0.5;
+    
     return ampl1 * (3 * sin(v.coord[0].val * (abs(cos(v.coord[0].val + Pi / 3) / 5 + 1))) + ampl2 * cos(v.coord[0].val * 20));
 }
 
@@ -41,7 +42,7 @@ double Function4(Argument v)    // Имитация голоса 2
     return abs(8 * sin(v.coord[0].val / 2) + 2 * cos(2 * v.coord[0].val) + (1 + abs(cos(2 * v.coord[0].val))) * sin(20 * v.coord[0].val));
 }
 
-int sign(double x)
+int sign(double x)              // Знак числа
 {
     if (x > 0)
     {
@@ -51,10 +52,11 @@ int sign(double x)
     {
         return -1;
     }
+    
     return 0;
 }
 
-double DecreaseTemp(int step)   // Ф-ия изменения температуры
+double DecreaseTemp(int step)   // Функция изменения температуры
 {
     return T_start * pow(0.95, step);
 }
@@ -83,9 +85,10 @@ Argument GenerateRandomPos(Argument v, double T)  // Генерация случ
     return temp;
 }
 
-bool HaveEvent(double chance)       // Выдает true с вероятностью chance
+bool HaveEvent(double chance)   // Выдает true с вероятностью chance
 {
     double event = (double)(rand()) / RAND_MAX;
+    
     if (event < chance)
     {
         return true;
@@ -104,7 +107,6 @@ double TransitionChance(double dE, double temp) // Вероятность пер
 Argument NewPosition(double (*f)(Argument), Argument v_old, double T)
 {
     double dE = 0, E_old = f(v_old);
-    bool havechange = false;
     Argument v_new;
     
     do
@@ -129,19 +131,12 @@ std::ostream& operator<< (std::ostream &out, Argument x)
         }
     }
     out << "}";
+    
     return out;
 }
 
-int main()
+void Initialisation(Argument &v_start)
 {
-    Argument v_start, v, v_max;
-    int step = 0;
-    double T = 0;
-    double max_value = 0;
-    
-    srand(time(0));
-    
-    // -- Инициализация -- //
     cout << " > Write down left and right border of searching area : " << endl;
     for (int i = 0; i < D; i++)
     {
@@ -149,31 +144,62 @@ int main()
         cin >> v_start.coord[i].A >> v_start.coord[i].B;
         v_start.coord[i].val = v_start.coord[i].A + ((double)(rand()) / RAND_MAX) * (v_start.coord[i].B - v_start.coord[i].A);
     }
-    max_value = Function3(v_start);
+}
+
+double GlobalMax(double (*f)(Argument), Argument start, Argument &v_max)
+{
+    Argument v;
+    int step = 0;
+    double max = 0, f_val = 0, T = T_start;
     
-    // -- Повышение точности результата -- //
-    for (int i = 0; i < repeat_num; i++)
+    // -- Метод имитации отжига -- //
+    max = f(start);
+    v = start;
+    while (T > T_end)
     {
-        v = v_start;
-        T = T_start;
-        step = 0;
-        
-        // -- Имитация отжига -- //
-        while (T > T_end)
+        step++;
+        T = DecreaseTemp(step);
+        v = NewPosition(f, v, T);
+        f_val = f(v);
+        if (max < f_val)
         {
-            step++;
-            T = DecreaseTemp(step);
-            v = NewPosition(Function3, v, T);
-            if (max_value < Function3(v))
-            {
-                max_value = Function3(v);
-                v_max = v;
-            }
+            max = f_val;
+            v_max = v;
         }
     }
+    // -- //
     
-    cout << " > Maximum point : " << v_max << endl;
-    cout << " > Maximum value : " << max_value << endl;
+    return max;
+}
+
+int main()
+{
+    Argument v_start, v_max, precise_v_max;
+    double precise_max_value = 0, max_value = 0;
+    
+    // -- Инициализируем генератор случайных чисел -- //
+    srand(time(0));
+    // -- //
+    
+    // -- Инициализация начальной позиции и границ -- //
+    Initialisation(v_start);
+    max_value = Function3(v_start);
+    // -- //
+    
+    // -- Повышение точности результата -- //
+    for (int i = 0; i < Repeat_num; i++)
+    {
+        max_value = GlobalMax(Function3, v_start, v_max);
+        if (max_value > precise_max_value || i == 0)
+        {
+            precise_max_value = max_value;
+            precise_v_max = v_max;
+        }
+    }
+    // -- //
+    
+    cout << " > Maximum point : " << precise_v_max << endl;
+    cout << " > Maximum value : " << precise_max_value << endl;
     
     system("Pause");
     return 0;
