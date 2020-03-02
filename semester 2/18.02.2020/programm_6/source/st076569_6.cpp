2870aef0f13293ece6a36ed1214dff129b4cf68f
st076569 <55051748+st076569@users.noreply.github.com>
2020-03-03 02:10:34 +0300

    Add files via upload

---
+++ semester 2/18.02.2020/programm_6/source/st076569_6.cpp
@@ -0,0 +1,24 @@
+﻿// написать программу, находящую кол-во делителей натурального числа
+
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main() 
+{
+	int x;
+    
+	cin >> x;		//Вводим число
+	if (Simple(x)) 		//Проверяем на простоту
+	{
+        cout << "Simple" << endl;
+        cout << 1 << " " << x << endl;
+	} else 
+	{ 
+        cout << "Compound" << endl;
+        Divisors(x);
+	}
+
+	return 0;
+}
